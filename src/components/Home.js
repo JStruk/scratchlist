2d85becc7620e1b7491d1f3e527e9e9b33e396da
JStruk <strukjustin@gmail.com>
2022-05-30 16:36:56 -0400

    use styled components on the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import TypeWriterEffect from 'react-typewriter-effect';
+import tw from "tailwind-styled-components";
 
 const ListTypes = [
     'ToDo List',
@@ -19,37 +20,45 @@ export default function Home() {
         navigate('/list')
     }
 
+    const HomeContainer = tw.div`min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12`
+    const Content = tw.div`relative py-3 sm:max-w-xl sm:mx-auto`
+    const GradientBackgroundCard = tw.div`absolute inset-0 bg-gradient-to-r from-teal-300 to-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl`
+    const MainCard = tw.div`relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20`
+    const MainCardContent = tw.div`max-w-md mx-auto`
+    const Header = tw.span`text-center text-5xl text-teal-400`
+    const TypeWriterContainer = tw.div`py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7`
+    const ActionButtonContainer = tw.div`pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7`
+    const GetStartedButton = tw.button`text-cyan-600 hover:text-cyan-700 border rounded-xl bg-teal-400 p-4`
+
     return (
-        <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-            <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-                <div className="absolute inset-0 bg-gradient-to-r from-teal-300 to-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl" />
-                <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-                    <div className="max-w-md mx-auto">
-                        <div className="text-center">
-                            <h1 className="text-5xl text-teal-400"> ScratchList </h1>
-                        </div>
+        <HomeContainer>
+            <Content>
+                <GradientBackgroundCard/>
+                <MainCard>
+                    <MainCardContent>
+                        <Header> ScratchList </Header>
                         <div className="divide-y divide-gray-200 text-center">
-                            <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                            <TypeWriterContainer>
                                 <p>The one-stop application for </p>
                                 <TypeWriterEffect
-                                    textStyle={{
+                                    textStyle={ {
                                         'textAlign': 'center'
-                                    }}
+                                    } }
                                     cursorColor="#3F3D56"
-                                    multiText={ListTypes}
-                                    multiTextDelay={1000}
+                                    multiText={ ListTypes }
+                                    multiTextDelay={ 1000 }
                                     multiTextLoop
-                                    typeSpeed={100}
+                                    typeSpeed={ 100 }
                                 />
-                            </div>
-                            <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
-                                <button onClick={createNewList} className="text-cyan-600 hover:text-cyan-700 border rounded-xl bg-teal-400 p-4"> Get Started! &rarr;</button>
-                            </div>
+                            </TypeWriterContainer>
+                            <ActionButtonContainer className="">
+                                <GetStartedButton onClick={ createNewList }> Get Started! &rarr;</GetStartedButton>
+                            </ActionButtonContainer>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    </MainCardContent>
+                </MainCard>
+            </Content>
+        </HomeContainer>
     );
 }
 
