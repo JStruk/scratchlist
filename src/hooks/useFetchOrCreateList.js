410a6624ac762cde8ade54af70fbd5fdf5700f82
JStruk <strukjustin@gmail.com>
2022-05-30 16:24:42 -0400

    refactor useFetchOrCreateList

--- src/hooks/useFetchOrCreateList.js
+++ src/hooks/useFetchOrCreateList.js
@@ -15,23 +15,21 @@ export const useFetchOrCreateList = (setListId, setTodos, todos) => {
         const listIdFromParams = query.get('listId')
         const fetchListFromApi = async (listId) => {
             let list;
-            if (listId) {
-                console.log('hi');
-                list = await fetchList(listIdFromParams, setListId, setTodos) || await createListWithId(listIdFromParams, setListId, setTodos, navigate)
-                setListId(listId)
-                if (list.listItems) {
-                    setTodos(list.listItems.sort((a, b) => a.id > b.id))
-                }
-                return
+            listId
+                ? list = await fetchList(listIdFromParams) || await createListWithId(listIdFromParams, navigate)
+                : list = await createList(navigate)
+
+            setListId(list.id)
+            if (list.listItems) {
+                setTodos(list.listItems.sort((a, b) => a.id > b.id))
             }
-            await createList(setListId, navigate)
         }
 
         fetchListFromApi(listIdFromParams)
     }, [false])
 }
 
-const fetchList = async (listId, setListId, setTodos) => {
+const fetchList = async (listId) => {
     try {
         return (await axios.get(`/list?listId=${ listId }`)).data
     } catch (e) {
@@ -39,21 +37,18 @@ const fetchList = async (listId, setListId, setTodos) => {
     }
 }
 
-const createListWithId = async (listId, setListId, setTodos, navigate) => {
+const createListWithId = async (listId, navigate) => {
     const newListPayload = {
         id: listId,
-        items: []
+        listItems: []
     }
     const list = (await axios.post('/list', newListPayload)).data
     navigate(`?listId=${ listId }`)
     return list
 }
 
-const createList = async (setListId, navigate) => {
-    const newListPayload = {
-        items: []
-    }
-    const newList = (await axios.post('/list', newListPayload)).data
+const createList = async (navigate) => {
+    const newList = (await axios.post('/list')).data
     navigate(`?listId=${ newList.id }`)
     return newList
 }
