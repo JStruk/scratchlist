4aa698ac2a245397c27299e3e14cc32324c6ccca
JStruk <strukjustin@gmail.com>
2022-06-24 15:21:58 -0400

    migrate index file to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,7 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 import './assets/index.css';
-import { createRoot, Root } from "react-dom/client";
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 
 // const rootElement = document.getElementById("root");
 // const root: Root = createRoot(rootElement);
@@ -12,10 +11,9 @@ import ReactDOM from 'react-dom';
 //     <App />
 // );
 
-ReactDOM.render(
-    <></>,
-    document.getElementById('root')
-)
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
